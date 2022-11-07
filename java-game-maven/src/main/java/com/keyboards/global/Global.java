6f352247c6fd8ca7ac461957e2358a980e05903b
Audrey <audreytraore497@gmail.com>
2022-11-07 18:06:53 +0100

    added Global class

---
+++ java-game-maven/src/main/java/com/keyboards/global/Global.java
@@ -0,0 +1,14 @@
+package com.keyboards.global;
+
+public class Global {
+	public static final int ORIGINAL_TILE_SIZE = 16;
+	public static final int SCALE = 3;
+	public static final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+	public static final int COL_NUM = 16;
+	public static final int ROW_NUM = 12;
+	
+	public static final int WIDTH = COL_NUM * TILE_SIZE;
+	public static final int HEIGHT = ROW_NUM * TILE_SIZE;
+	
+	public static boolean DEBUG = false;
+}
