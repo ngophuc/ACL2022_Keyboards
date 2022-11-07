b925d10064f482cbb404359f3e3693ed91ab7b17
Audrey <audreytraore497@gmail.com>
2022-11-07 17:55:15 +0100

    added tiles and tile manager

---
+++ java-game-maven/src/main/java/com/keyboards/tile/Tile.java
@@ -0,0 +1,44 @@
+package tile;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+import game.Entity;
+import global.Global;
+
+public class Tile {
+	
+	public BufferedImage image;
+	public Rectangle solidBox;
+	
+	public Tile(BufferedImage image) {
+		this.image = image;
+		this.solidBox = null;
+	}
+	
+	public Tile(BufferedImage image, Rectangle solidBox) {
+		this.image = image;
+		this.solidBox = solidBox;
+	}
+	
+	public boolean isSolid() {
+		return solidBox != null;
+	}
+
+	public boolean collidsWith(Rectangle solidBox) {
+		
+		return this.solidBox.intersects(solidBox);
+	}
+	
+	public void draw(Graphics2D g, int col, int row) {
+		g.drawImage(this.image, col * Global.TILE_SIZE, row * Global.TILE_SIZE, Global.TILE_SIZE, Global.TILE_SIZE, null);
+		// draw the solid box
+		g.setColor(Color.PINK);
+		if (this.solidBox != null) {
+			g.drawRect(this.solidBox.x,this.solidBox.y, this.solidBox.width, this.solidBox.height);
+		}
+	}
+	
+}
---
+++ java-game-maven/src/main/java/com/keyboards/tile/TileManager.java
@@ -0,0 +1,79 @@
+package tile;
+
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.lang.model.element.NestingKind;
+
+import global.Global;
+
+public class TileManager {
+	
+	public ArrayList<Tile> tiles;
+	public Tile[][] mapTiles;
+
+	public TileManager() {
+		tiles = new ArrayList<Tile>();
+		mapTiles = new Tile[Global.COL_NUM + 1][Global.ROW_NUM + 1];
+		
+		loadTileArrayFromDir("/res/tiles");
+	}
+	
+	private BufferedImage loadTileImage(File file) {
+		BufferedImage tileImage = null;
+        try {
+			System.out.println("Loading: " + file + "...");
+            tileImage = ImageIO.read(file);
+        } catch (Exception e) {
+            System.out.println("ERROR: could not load file: " + file);
+            System.err.println(e);
+        }
+        return tileImage;
+	}
+	
+	// need to figure out a way to properly order the tiles
+	private void loadTileArrayFromDir(String dirPath) {
+		File dir = new File(dirPath);
+		if (dir.isDirectory()) {
+			File[] files = dir.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				tiles.add(new Tile(loadTileImage(files[i]), new Rectangle(0, 0, Global.TILE_SIZE, Global.TILE_SIZE/2)));
+			}
+		} else {
+			System.out.println("ERROR: not a directory: " + dirPath);
+		}
+	}
+	
+	public Tile geTile(int col, int row) {
+		return mapTiles[row][col];
+	}
+
+	public void draw(Graphics2D g) {
+		for (int i = 0; i < tiles.size(); i++) {
+			Tile tile = tiles.get(i);
+			if (tile != null) {
+				if (i == 3) {
+					mapTiles[i+1][5] = tile;
+					tile.solidBox.x = (i+1) * Global.TILE_SIZE;
+				} else {
+					mapTiles[i][5] = tile;
+					tile.solidBox.x = i * Global.TILE_SIZE;
+				}
+				tile.solidBox.y = 5 * Global.TILE_SIZE + Global.TILE_SIZE/2;
+			}
+		}
+
+		for (int i = 0; i < Global.COL_NUM; i++) {
+			for (int j = 0; j < Global.ROW_NUM; j++) {
+				if (mapTiles[i][j] != null) {
+					mapTiles[i][j].draw(g, i, j);
+				}
+			}
+		}
+	}
+
+}
