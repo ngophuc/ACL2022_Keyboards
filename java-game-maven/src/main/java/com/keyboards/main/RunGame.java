93423d1d89ec07298b4bf22895c0d2328c038d09
Swoqa <96593792+Swoqa@users.noreply.github.com>
2022-11-25 01:02:08 +0100

    Add files via upload
    Modification du rungame pour la mise à jour des objets

---
+++ java-game-maven/src/main/java/com/keyboards/main/RunGame.java
@@ -0,0 +1,137 @@
+package com.keyboards.main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.keyboards.engine.Game;
+import com.keyboards.game.Entity;
+import com.keyboards.game.Ghost;
+import com.keyboards.game.Player;
+import com.keyboards.game.Potion_attaque;
+import com.keyboards.game.Potion_bouclier;
+import com.keyboards.game.Potion_vie;
+import com.keyboards.game.Potion_vitesse;
+import com.keyboards.game.Treasure;
+import com.keyboards.game.Zombie;
+import com.keyboards.tile.TileManager;
+
+public class RunGame implements Game {
+	
+	ArrayList<Entity> entities = new ArrayList<Entity>();
+
+	TileManager tileManager = new TileManager();
+	
+	Player player = new Player(tileManager.mapTiles);
+	Ghost ghost = new Ghost(tileManager.mapTiles);
+	Zombie zombie = new Zombie(tileManager.mapTiles);
+	Treasure tresor = new Treasure(tileManager.mapTiles,15,player);
+	Potion_vie potion_vie=new Potion_vie(tileManager.mapTiles,15,player);
+	Potion_attaque potion_attaque=new Potion_attaque(tileManager.mapTiles,15,player);
+	Potion_bouclier potion_bouclier=new Potion_bouclier(tileManager.mapTiles,15,player);
+	Potion_vitesse potion_vitesse=new Potion_vitesse(tileManager.mapTiles,15,player);
+	/**
+	 * constructeur avec fichier source pour le help
+	 * 
+	 */
+	public RunGame(String source) {
+		BufferedReader helpReader;
+		try {
+			helpReader = new BufferedReader(new FileReader(source));
+			String ligne;
+			while ((ligne = helpReader.readLine()) != null) {
+				System.out.println(ligne);
+			}
+			helpReader.close();
+		} catch (IOException e) {
+			System.out.println("Help not available");
+		}
+		
+		entities.add(player);
+		entities.add(zombie);
+		entities.add(ghost);
+		entities.add(tresor);
+		entities.add(potion_vie);
+		entities.add(potion_attaque);
+		entities.add(potion_vitesse);
+		entities.add(potion_bouclier);
+	}
+	
+	/**
+	 * faire evoluer le jeu suite a une commande
+	 * 
+	 * @param commande
+	 */
+	@Override
+	public void evolve(HashMap<String, Boolean> commands) {
+		// String commandsString = "";
+		
+		if (commands.get("UP")) {
+			// commandsString += "UP ";
+
+			player.moveUp();
+		}
+		if (commands.get("DOWN")) {
+			// commandsString += "DOWN ";
+
+			player.moveDown();
+		}
+		if (commands.get("LEFT")) {
+			// commandsString += "LEFT ";
+
+			player.moveLeft();
+		}
+		if (commands.get("RIGHT")) {
+			// commandsString += "RIGHT ";
+
+			player.moveRight();
+		}
+		if (commands.get("SHIFT")) {
+			player.isSprinting = true;
+		} else {
+			player.isSprinting = false;
+		}
+		if (commands.get("SPACE")) {
+			// commandsString += "SPACE ";
+			// TODO : use object
+		}
+		if (Controller.isIdle(commands)) {
+			// commandsString += "IDLE ";
+			player.idle();
+		}
+
+		// System.out.println("Commandes : " + commandsString);
+		
+		player.spriteCounter++;
+		if (player.spriteCounter > 5) {
+			if (player.spriteNum < player.NUMBER_OF_FRAME_IN_WALK_ANIM - 1) {
+				player.spriteNum++;
+			} else {
+				player.spriteNum = 0;
+			}
+			player.spriteCounter = 0;
+		}
+		
+		zombie.moveTowards(player);
+		if (zombie.collidesWith(player)) {
+			zombie.attack(player);
+		}
+		// ghost.moveTowards(player);
+		// if (ghost.collidesWith(player)) {
+		// 	ghost.attack(player);
+		// }
+		
+//		System.out.println("sprite counter : " + player.spriteCounter + " sprite num : " + player.spriteNum);
+	}
+
+	/**
+	 * verifier si le jeu est fini
+	 */
+	@Override
+	public boolean isFinished() {
+		// le jeu n'est jamais fini
+		return false;
+	}
+}
